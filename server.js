4cabf0d2c2ea334036990d6f8a75bb4ed670d721
practicebysk <143207763+practicebysk@users.noreply.github.com>
2024-10-16 21:26:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const path = require("path");
 
 const server = jsonServer.create();
-const router1 = jsonServer.router(path.join(__dirname, "db.json"));
+const router1 = jsonServer.router(path.join(__dirname, "db1.json"));
 const router2 = jsonServer.router(path.join(__dirname, "db2.json"));
 const middlewares = jsonServer.defaults();
 
@@ -12,11 +12,10 @@ server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 
-// Use routers with different routes
-server.use("/api1", router1);  // Access db.json via /api1
-server.use("/api2", router2);  // Access db2.json via /api2
+server.use("/travel", router1); 
+server.use("/portfolio", router2); 
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on http://localhost:${port}`);
 });
